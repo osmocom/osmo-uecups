f9e9a7022e9502dad97653d91ba2ae4fa7926d2a
Harald Welte <laforge@osmocom.org>
2020-04-15 22:04:00 +0200

    add jenkins.sh
    Change-Id: I5de3a4ee209ae78346aaa4ce5734b5ef213e5c07

---
+++ contrib/jenkins.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+# jenkins build helper script for osmocom-bb.  This is how we build on jenkins.osmocom.org
+
+set -ex
+
+base="$PWD"
+deps="$base/deps"
+inst="$deps/install"
+export deps inst
+
+osmo-clean-workspace.sh
+
+mkdir "$deps" || true
+
+osmo-build-dep.sh libosmocore "" ac_cv_path_DOXYGEN=false
+osmo-build-dep.sh libosmo-abis "" ac_cv_path_DOXYGEN=false
+osmo-build-dep.sh libosmo-netif "" ac_cv_path_DOXYGEN=false
+
+export PKG_CONFIG_PATH="$inst/lib/pkgconfig:$PKG_CONFIG_PATH"
+export LD_LIBRARY_PATH="$inst/lib"
+
+set +x
+echo
+echo
+echo
+echo " =============================== osmo-uecups ============================="
+echo
+set -x
+
+cd daemon
+make
+
+osmo-clean-workspace.sh
+
