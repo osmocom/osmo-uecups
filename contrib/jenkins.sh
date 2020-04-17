7dadaa921f2468fca7e5eb931a7f3e6742d384cb
Harald Welte <laforge@osmocom.org>
2020-04-17 15:07:48 +0200

    jenkins.sh: Fix PKG_CONFIG_DIR
    Change-Id: Ie2391ca00e588ddc9b7920dbf28ac69ddd7173e0

--- contrib/jenkins.sh
+++ contrib/jenkins.sh
@@ -12,6 +12,12 @@ osmo-clean-workspace.sh
 
 mkdir "$deps" || true
 
+export PKG_CONFIG_PATH="$inst/lib/pkgconfig:$PKG_CONFIG_PATH"
+export LD_LIBRARY_PATH="$inst/lib"
+export PATH="$inst/bin:$PATH"
+
+verify_value_string_arrays_are_terminated.py $(find . -name "*.[hc]")
+
 osmo-build-dep.sh libosmocore "" ac_cv_path_DOXYGEN=false
 osmo-build-dep.sh libosmo-abis "" ac_cv_path_DOXYGEN=false
 osmo-build-dep.sh libosmo-netif "" ac_cv_path_DOXYGEN=false
